27d6ab2fca97d26dd2227ad5126097a46ca436f5
Alejandro Barreto <alejandro.barreto@ni.com>
2019-06-26 15:22:01 -0500

    Rev the version to include the new base selector merge base option

--- src/azdo-pr-dashboard.user.js
+++ src/azdo-pr-dashboard.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 
 // @name         AzDO Pull Request Improvements
-// @version      2.14.0
+// @version      2.15.0
 // @author       National Instruments
 // @description  Adds sorting and categorization to the PR dashboard. Also adds minor improvements to the PR diff experience, such as a base update selector and per-file checkboxes.
 // @license      MIT
