6dea00f40ca09a2d835676dd3ecd11cc3b619d9b
Alejandro Barreto <alejandro.barreto@ni.com>
2019-06-26 19:25:10 -0500

    Rev the version again

--- src/azdo-pr-dashboard.user.js
+++ src/azdo-pr-dashboard.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 
 // @name         AzDO Pull Request Improvements
-// @version      2.16.0
+// @version      2.17.0
 // @author       National Instruments
 // @description  Adds sorting and categorization to the PR dashboard. Also adds minor improvements to the PR diff experience, such as a base update selector and per-file checkboxes.
 // @license      MIT
