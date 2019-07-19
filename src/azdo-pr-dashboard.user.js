7e9b325600d178579eac0c85e79e514fcb5744d8
Alejandro Barreto <alejandro5042@gmail.com>
2019-07-19 14:17:19 -0500

    Only bump version
    Need to bump the version to test redirecting the update link to the GH release link vs. directly from master.

--- src/azdo-pr-dashboard.user.js
+++ src/azdo-pr-dashboard.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 
 // @name         AzDO Pull Request Improvements
-// @version      2.20.2
+// @version      2.20.3
 // @author       Alejandro Barreto (National Instruments)
 // @description  Adds sorting and categorization to the PR dashboard. Also adds minor improvements to the PR diff experience, such as a base update selector and per-file checkboxes.
 // @license      MIT
