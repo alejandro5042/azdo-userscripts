cacbcb2495a30cb1e3ec0338173228933277a6dd
Alejandro Barreto <alejandro.barreto@ni.com>
2019-07-20 14:47:05 -0500

    Remove function and use strict since Parcel does that for us

--- src/azdo-pr-dashboard.user.js
+++ src/azdo-pr-dashboard.user.js
@@ -3,27 +3,23 @@ import * as prDashboard from './pr-dashboard';
 import * as prFileCheckboxes from './pr-file-checkboxes';
 import * as baseUpdateSelector from './base-update-selector';
 
-(function () {
-  'use strict';
+// All REST API calls should fail after a timeout, instead of going on forever.
+$.ajaxSetup({ timeout: 5000 });
 
-  // All REST API calls should fail after a timeout, instead of going on forever.
-  $.ajaxSetup({ timeout: 5000 });
+// Set a namespace for our local storage items.
+lscache.setBucket('acb-azdo/');
 
-  // Set a namespace for our local storage items.
-  lscache.setBucket('acb-azdo/');
+// Call our event handler if we notice new elements being inserted into the DOM. This happens as the page is loading or updating dynamically based on user activity. We throttle new element events to avoid using up CPU when AzDO is adding a lot of elements during a short time (like on page load).
+document.addEventListener('DOMNodeInserted', _.throttle(onPageDOMNodeInserted, 400));
 
-  // Call our event handler if we notice new elements being inserted into the DOM. This happens as the page is loading or updating dynamically based on user activity. We throttle new element events to avoid using up CPU when AzDO is adding a lot of elements during a short time (like on page load).
-  document.addEventListener('DOMNodeInserted', _.throttle(onPageDOMNodeInserted, 400));
-
-  // This is "main()" for this script. Runs periodically when the page updates.
-  function onPageDOMNodeInserted(event) {
-    // The page may not have refreshed when moving between URLs--sometimes AzDO acts as a single-page application. So we must always check where we are and act accordingly.
-    if (/\/(pullrequest)\//i.test(window.location.pathname)) {
-      prFileCheckboxes.addCheckboxesToFiles();
-      baseUpdateSelector.addBaseUpdateSelector();
-      betterScrollbars.makePullRequestDiffEasierToScroll();
-    } else if (/\/(_pulls|pullrequests)/i.test(window.location.pathname)) {
-      prDashboard.sortPullRequestDashboard();
-    }
+// This is "main()" for this script. Runs periodically when the page updates.
+function onPageDOMNodeInserted(event) {
+  // The page may not have refreshed when moving between URLs--sometimes AzDO acts as a single-page application. So we must always check where we are and act accordingly.
+  if (/\/(pullrequest)\//i.test(window.location.pathname)) {
+    prFileCheckboxes.addCheckboxesToFiles();
+    baseUpdateSelector.addBaseUpdateSelector();
+    betterScrollbars.makePullRequestDiffEasierToScroll();
+  } else if (/\/(_pulls|pullrequests)/i.test(window.location.pathname)) {
+    prDashboard.sortPullRequestDashboard();
   }
-}());
+}
