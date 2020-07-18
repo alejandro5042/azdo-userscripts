e79cc065282b75e6049ab8a38c5f2a70634f4966
Alejandro Barreto <alejandro.barreto@ni.com>
2020-07-17 19:10:29 -0500

    Move bug annotation so that the file/build annotations are always aligned

--- src/azdo-pr-dashboard.user.js
+++ src/azdo-pr-dashboard.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 
 // @name         AzDO Pull Request Improvements
-// @version      2.44.0
+// @version      2.44.1
 // @author       Alejandro Barreto (National Instruments)
 // @description  Adds sorting and categorization to the PR dashboard. Also adds minor improvements to the PR diff experience, such as a base update selector and per-file checkboxes.
 // @license      MIT
@@ -826,12 +826,15 @@
         const title = row.querySelector('.body-l');
         title.insertAdjacentHTML('afterend', blockingAnnotation);
       }
+    }
 
+    await annotateBugsOnPullRequestRow(row, pr);
+
+    if (sectionTitle === 'Assigned to me') {
       await annotateFileCountOnPullRequestRow(row, pr);
     }
 
     await annotateBuildStatusOnPullRequestRow(row, pr);
-    await annotateBugsOnPullRequestRow(row, pr);
   }
 
   function countVotes(pr) {
