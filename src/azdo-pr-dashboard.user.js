64f5263d9c68a3d3a0afa28da649d65adc6067ad
Alejandro Barreto <alejandro.barreto@ni.com>
2021-04-09 13:05:02 -0500

    Get rid of base selector; it's built-in now!

--- src/azdo-pr-dashboard.user.js
+++ src/azdo-pr-dashboard.user.js
@@ -142,7 +142,6 @@
     try {
       // The page may not have refreshed when moving between URLs--sometimes AzDO acts as a single-page application. So we must always check where we are and act accordingly.
       if (/\/(pullrequest)\//i.test(window.location.pathname)) {
-        addBaseUpdateSelector();
         makePullRequestDiffEasierToScroll();
         applyStickyPullRequestComments();
         highlightAwaitComments();
@@ -644,70 +643,6 @@
         /* We borrowed padding from the diff to give to the bottom of the file row. So adjust accordingly (this value was originally 20px). */
         padding-top: 10px;
       }`);
-  }
-
-
-  // If we're on specific PR, add a base update selector.
-  function addBaseUpdateSelector() {
-    $('.vc-iteration-selector').once('add-base-selector').each(async function () {
-      const toolbar = $(this);
-
-      addStyleOnce('base-selector-css', /* css */ `
-        .base-selector {
-          color: var(--text-secondary-color);
-          margin: 0px 5px 0px 0px;
-        }
-        .base-selector select {
-          border: 1px solid transparent;
-          padding: 2px 4px;
-          width: 3em;
-          height: 100%;
-          text-align: center;
-        }
-        .base-selector select:hover {
-          border-color: var(--palette-black-alpha-20);
-        }
-        .base-selector select option {
-          background: var(--callout-background-color);
-          color: var(--text-primary-color);
-          font-family: Consolas, monospace;
-        }
-        .base-selector select option:disabled {
-          display: none;
-        }`);
-
-      // Get the PR iterations.
-      const prUrl = await getCurrentPullRequestUrlAsync();
-      const iterations = (await $.get(`${prUrl}/iterations?api-version=5.0`)).value;
-
-      // Create a dropdown with the first option being the icon we show to users. We use an HTML dropdown since its much easier to code than writing our own with divs/etc or trying to figure out how to use an AzDO dropdown.
-      const selector = $('<select><option value="" disabled selected>↦</option></select>');
-
-      // Add an option for each iteration in the dropdown, looking roughly the same as the AzDO update selector.
-      for (const iteration of iterations.reverse()) {
-        const date = Date.parse(iteration.createdDate);
-        const truncatedDescription = truncate(iteration.description);
-        const optionText = `Update ${iteration.id.toString().padEnd(4)} ${truncatedDescription.padEnd(61)} ${dateFns.distanceInWordsToNow(date).padStart(15)} ago`;
-        $('<option>').val(iteration.id).text(optionText).appendTo(selector);
-      }
-
-      // Add the last option to select the merge base as the diff base (essentially update zero).
-      $('<option value="0">            === Merge Base ===</option>').appendTo(selector);
-
-      // Replace spaces with non-breaking spaces (char 0xa0) to force the browser to not collapse it so that we can align the dates to the right of the dropdown. Apprently even `white-space: pre !important;` doesn't work on `option` element css.
-      selector.children('option').each(function () { $(this).text((i, text) => text.replace(/ /g, '\xa0')); });
-
-      // Finally add the dropdown to the toolbar.
-      $('<div class="base-selector" />').append(selector).prependTo(toolbar);
-
-      // When an option is selected, update the URL to include the selected base update.
-      selector.on('change', function (event) {
-        const currentUrl = new URL(window.location.href);
-        currentUrl.searchParams.set('base', $(this).first().val());
-        currentUrl.searchParams.set('iteration', currentUrl.searchParams.get('iteration') || iterations.length); // If we select a base without having an explicit iteration, compare the base to the latest.
-        window.location.href = currentUrl.toString();
-      });
-    });
   }
 
   addStyleOnce('pr-dashboard-css', /* css */ `
