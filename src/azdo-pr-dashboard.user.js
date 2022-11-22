3a1cd40c393dcb98ee75bf78ab3f127841efad5a
Humberto Garza <humberto-garza@users.noreply.github.com>
2022-11-22 13:37:21 -0600

    bump version

--- src/azdo-pr-dashboard.user.js
+++ src/azdo-pr-dashboard.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 
 // @name         More Awesome Azure DevOps (userscript)
-// @version      3.4.4
+// @version      3.4.5
 // @author       Alejandro Barreto (NI)
 // @description  Makes general improvements to the Azure DevOps experience, particularly around pull requests. Also contains workflow improvements for NI engineers.
 // @license      MIT
