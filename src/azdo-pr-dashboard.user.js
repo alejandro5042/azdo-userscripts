5205e78208064e09324c148d6bbf2a2e1b064b88
kroeschl <60117794+kroeschl@users.noreply.github.com>
2023-09-06 14:07:23 -0500

    Bump version to 3.6.1 (#230)

--- src/azdo-pr-dashboard.user.js
+++ src/azdo-pr-dashboard.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 
 // @name         More Awesome Azure DevOps (userscript)
-// @version      3.6.0
+// @version      3.6.1
 // @author       Alejandro Barreto (NI)
 // @description  Makes general improvements to the Azure DevOps experience, particularly around pull requests. Also contains workflow improvements for NI engineers.
 // @license      MIT
