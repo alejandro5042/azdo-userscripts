c9e5f75b58e713616db5257afcc7bc3ff713eb79
KC Hawes-Domingue <kc.hawesdomingue@emerson.com>
2026-04-17 10:57:40 -0500

    Fix invisible scroll bar on release pipeline logs (#238)
    * Fix invisible scroll bar on release pipeline logs
    
    * Update version
    
    * Don't bother with themes - log window is always black
    
    ---------
    
    Co-authored-by: Alejandro Barreto <alejandro5042@gmail.com>

--- src/azdo-pr-dashboard.user.js
+++ src/azdo-pr-dashboard.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 
 // @name         More Awesome Azure DevOps (userscript)
-// @version      3.8.3
+// @version      3.9.0
 // @author       Alejandro Barreto (NI)
 // @description  Makes general improvements to the Azure DevOps experience, particularly around pull requests. Also contains workflow improvements for NI engineers.
 // @license      MIT
@@ -142,6 +142,10 @@
     eus.onUrl(/\/(_git)/gi, (session, urlMatch) => {
       doEditAction(session);
       watchForRepoBrowsingPages(session);
+    });
+
+    eus.onUrl(/\/(_releaseProgress)/gi, (session, urlMatch) => {
+      fixScrollBarColor();
     });
 
     // Throttle page update events to avoid using up CPU when AzDO is adding a lot of elements during a short time (like on page load).
@@ -1384,6 +1388,10 @@
         $(elm).append(section);
       }
     });
+  }
+
+  function fixScrollBarColor() {
+    GM_addStyle('.custom-scrollbar { scrollbar-color: rgb(99 99 99) black !important; }');
   }
 
   async function watchForLVDiffsAndAddNIBinaryDiffButton(session) {
